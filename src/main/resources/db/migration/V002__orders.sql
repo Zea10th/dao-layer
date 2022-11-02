bab2a11c226314d3b607139542fe4770bcbd38f2
Alexander Biezbardis <mr.biezbardis@gmail.com>
2022-11-02 13:08:58 +0200

    Added migrations provided by flyway

--- src/main/resources/db/migration/V002__orders.sql
+++ src/main/resources/db/migration/V002__orders.sql
@@ -1,13 +1,3 @@
-create table customers
-(
-    id           serial      not null,
-    name         varchar(20) not null,
-    surname      varchar(30) not null,
-    age          int         not null,
-    phone_number varchar(15) default '+7901xxxyyzz',
-    primary key (id)
-);
-
 create table orders
 (
     id           serial       not null,
--- src/main/resources/db/migration/V004__orders_data.sql
+++ src/main/resources/db/migration/V004__orders_data.sql
@@ -1,16 +1,3 @@
-insert into customers (name, surname, age)
-values ('Vasya', 'Petrov', cast(random() * 50 + 18 as int)),
-       ('Olya', 'Kuznetsova', cast(random() * 50 + 18 as int)),
-       ('Alexey', 'Zhukov', cast(random() * 50 + 18 as int)),
-       ('Sveta', 'Malinina', cast(random() * 50 + 18 as int)),
-       ('Kirill', 'Sidorov', cast(random() * 50 + 18 as int)),
-       ('Masha', 'Bobkova', cast(random() * 50 + 18 as int)),
-       ('Alexey', 'Bezrukov', cast(random() * 50 + 18 as int)),
-       ('Sergey', 'Popov', cast(random() * 50 + 18 as int)),
-       ('Ira', 'Kozlova', cast(random() * 50 + 18 as int)),
-       ('alExey', 'Afonin', cast(random() * 50 + 18 as int)),
-       ('Anna', 'Ivanova', cast(random() * 50 + 18 as int));
-
 insert into orders (date, customer_id, product_name, amount)
 values (current_date - cast(random() * 100 as int),
         cast(random() * 10 + 1 as int), 'pencil',
